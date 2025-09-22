b5463893c043e7c1ba0f2c66c2e7e4e6b9f7d162
Matheus Batista Xavier <101474699+Matheusbxavier@users.noreply.github.com>
2025-09-22 12:59:09 -0300

    commit

--- bin/representando_quartos_com_herança.dart
+++ bin/representando_quartos_com_herança.dart
@@ -1,22 +1,54 @@
-void main() {}
+void main() {
+  QuartoSimples quartoSimples = QuartoSimples(3);
+  quartoSimples.reservar();
+  quartoSimples.calcularValorTotal();
+  QuartoMedio quartoMedio = QuartoMedio(5);
+  quartoMedio.reservar();
+  quartoMedio.servirCafeDaManha();
+  quartoMedio.calcularValorTotal();
+  QuartoLuxo quartoLuxo = QuartoLuxo(7);
+  quartoLuxo.reservar();
+  quartoLuxo.servirCafeDaManha();
+  quartoLuxo.servicoDeQuarto();
+  quartoLuxo.calcularValorTotal();
+}
 
 class Quarto {
   String tipo;
   double valorDiaria;
-  int numeroDeDiarias;
+  int diarias;
 
-  Quarto(this.tipo, this.valorDiaria, this.numeroDeDiarias);
+  Quarto(this.diarias, this.valorDiaria, this.tipo);
+
+  void reservar() {
+    print("Reserva realizada para $diarias diarias no quarto $tipo.");
+  }
+
+  void calcularValorTotal() {
+    print("Total a pagar: R\$${diarias * valorDiaria}");
+  }
 }
 
 class QuartoSimples extends Quarto {
-  QuartoSimples(super.tipo, super.valorDiaria, super.numeroDeDiarias);
+  QuartoSimples(int diarias) : super(diarias, 80, 'Simples');
 }
 
 class QuartoMedio extends Quarto {
-  QuartoMedio(super.tipo, super.valorDiaria, super.numeroDeDiarias);
+  QuartoMedio(int diarias) : super(diarias, 250, 'Médio');
+
+  void servirCafeDaManha() {
+    print("Servindo cafe da manha no quarto $tipo.");
+  }
 }
 
 class QuartoLuxo extends Quarto {
-  QuartoLuxo(super.tipo, super.valorDiaria, super.numeroDeDiarias);
-}
+  QuartoLuxo(int diarias) : super(diarias, 1000, 'Luxo');
 
+  void servirCafeDaManha() {
+    print("Servindo cafe da manha no quarto $tipo.");
+  }
+
+  void servicoDeQuarto() {
+    print("Limpando quarto $tipo.");
+  }
+}
