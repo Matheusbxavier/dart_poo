ba6dc2686f784ed30032a5b98986a2d986d26666
Matheus Batista Xavier <101474699+Matheusbxavier@users.noreply.github.com>
2025-09-30 13:50:33 -0300

    commit

--- bin/Implementando_controle_de_acesso_com_mixins.dart
+++ bin/Implementando_controle_de_acesso_com_mixins.dart
@@ -1,7 +1,26 @@
-mixin ControleDeAcesso {
-  verificarPermissaoAdmin(String papel) {
-    return papel == "admin";
+mixin ControleAcesso {
+  bool verificarPermissaoAdmin(String papel) {
+    return papel == 'admin';
+  }
+
+  void executarOperacao(String papel, Function operacao) {
+    if (verificarPermissaoAdmin(papel)) {
+      operacao();
+    } else {
+      print('Acesso negado');
+    }
   }
 }
 
+class ServicoUsuario with ControleAcesso {
+  void deletarUsuario() {
+    executarOperacao('admin', () => print('Usuário deletado'));
+  }
+}
+
+class ServicoProduto with ControleAcesso {
+  void adicionarProduto() {
+    executarOperacao('admin', () => print('Produto adicionado'));
+  }
+}
 void main() {}
